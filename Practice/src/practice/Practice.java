c73094e6253c86770d82ea21dc0bee5028df205e
Uttkarsh Goel <uttkarsh.goel@gmail.com>
2016-12-29 14:27:23 +0530

    plus 1

--- Practice/src/practice/Practice.java
+++ Practice/src/practice/Practice.java
@@ -17,5 +17,40 @@ public class Practice {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("I should start doing something interesting");
+		int rev = 1563847412;
+		int num = reverse(rev);
+		System.out.println("reversed num : " + num);
+		System.out.println("Value : " + String.valueOf(rev).charAt(0));
+    }
+	 public static int reverse(int x) {
+		int r, sum = 0;
+		int len = String.valueOf(x).length();
+        if(len>=10 && x>0){
+			System.out.println("1st");
+            return 0; 
+        }
+        else if((String.valueOf(x).charAt(0) > '2' || String.valueOf(x).charAt(len-1)>'2') 
+				 && String.valueOf(x).length()>10){
+			System.out.println("2nd");
+            return 0;
+        }
+        else{
+			System.out.println("3rd");
+            if(x>0){
+                while(x>0){
+                    r = x%10;
+                    sum = sum*10 + r;
+                    x = x/10;
+                }
+            }
+            else if(x<0){
+                while(x<0){
+                    r = x%10;
+                    sum = sum*10 + r;
+                    x = x/10;
+                }
+            }
+            return sum;
+        }
     }
 }
